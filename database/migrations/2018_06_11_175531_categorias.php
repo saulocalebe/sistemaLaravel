d5b583c53287a328f2477a690ad8a0bd641a6e77
Saulo Calebe <saulo.calebe@outlook.com>
2018-06-11 17:58:14 -0300

    Correção de migration

--- database/migrations/2018_06_11_175531_categorias.php
+++ database/migrations/2018_06_11_175531_categorias.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Categoria extends Migration
+class Categorias extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class Categoria extends Migration
      */
     public function up()
     {
-        Schema::create('categoria', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome');
             $table->string('descricao');
@@ -29,6 +29,6 @@ class Categoria extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categoria');
+        Schema::dropIfExists('categorias');
     }
 }
