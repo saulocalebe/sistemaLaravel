8b25d750c07a1302b991e2e5c86b0734b62a2ba0
Saulo Calebe <saulo.calebe@outlook.com>
2018-06-12 12:21:04 -0300

    Criado model categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $table = 'categorias';
+    protected $primarykey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome','descricao', 'condicao'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+    
+    protected $guarded = [
+
+    ];
 }
